0369b0215525cd567146cc4dfeb773d017c3f938
Heritage <ireanuoluwah@gmail.com>
2025-05-18 15:25:37 +0100

    searh Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,29 @@
-import {Box, Title} from '@mantine/core'
+import {Box, Text} from '@mantine/core'
 import HeroTitle from '../components/HeroTitle';
 export default function Home() {
   return (
-    <Box px={'lg'} bg={'blue.5'} style={{ 
-        backgroundColor: '#3b82f6', // blue-500 from Tailwind, or use Mantine's theme if desired
-        backgroundImage: `repeating-linear-gradient(
-          to right,
-          rgba(255, 255, 255, 0.2),  /* line color with opacity */
-          rgba(255, 255, 255, 0.2) 1px,
-          transparent 1px,
-          transparent 20px
-        )`,
-        height: '55vh', // adjust as needed
-    }}>
-      <HeroTitle text="Pitch your startup, connect with entrepreneurs"/>
+<Box
+  px="lg"
+  style={{
+    backgroundColor: '#3b82f6', // or use Mantine's theme.colorScheme if dynamic
+    backgroundImage: `repeating-linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0.2),
+      rgba(255, 255, 255, 0.2) 1px,
+      transparent 1px,
+      transparent 20px
+    )`,
+    height: '55vh',
+    display: 'flex',
+    alignItems:'center',
+    justifyContent: 'center', 
+    gap: '1rem',
+    flexDirection: 'column'
+  }}
+>
 
+      <HeroTitle text="Pitch your startup, connect with entrepreneurs"/>
+      <Text c={'white'} fz={'lg'}>Submit Ideas, vote on pitches and get noticed in virtual Competitions </Text>
     </Box>
   );
 }
--- app/components/HeroTitle.tsx
+++ app/components/HeroTitle.tsx
@@ -2,8 +2,8 @@ import {Box, Title} from '@mantine/core'
 const HeroTitle = ({text}: {text:String}) => {
   return (
     <>
-        <Box bg={'black'} w={'fit-content'} p={'lg'} m={'auto'} >
-            <Title style={{color:'white'}} order={1} ta={'center'} >
+        <Box  bg={'black'} w={'fit-content'} p={'lg'} >
+            <Title style={{color:'white', }} order={1} ta={'center'} >
                 {text.toUpperCase()}
             </Title>
         </Box>
